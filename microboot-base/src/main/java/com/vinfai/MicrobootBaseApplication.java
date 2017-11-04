b9a30ec49eb0635c29bb43e2006f08343e355084
vinfai85 <vinfai85@gmail.com>
2017-11-04 19:24:13 +0800

    init

---
+++ microboot-base/src/main/java/com/vinfai/MicrobootBaseApplication.java
@@ -0,0 +1,12 @@
+package com.vinfai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrobootBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrobootBaseApplication.class, args);
+	}
+}
